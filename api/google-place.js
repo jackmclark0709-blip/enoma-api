fb7ffce555d01845bfc5a3b3aa65b9b6a60d1a68
Jack Clark <Jack.Clark@assayquant.com>
2025-12-14 18:57:53 -0500

    trying to get good places API

--- api/google-place.js
+++ api/google-place.js
@@ -5,17 +5,24 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: "Missing place_id" });
   }
 
-  const url =
-    "https://maps.googleapis.com/maps/api/place/details/json" +
-    `?place_id=${place_id}` +
-    "&fields=name,rating,reviews,opening_hours,geometry,formatted_address" +
-    `&key=${process.env.GOOGLE_SERVER_PLACES_KEY}`;
-
   try {
+    const url =
+      "https://maps.googleapis.com/maps/api/place/details/json" +
+      `?place_id=${place_id}` +
+      `&fields=rating,reviews,opening_hours` +
+      `&key=${process.env.GOOGLE_SERVER_PLACES_KEY}`;
+
     const r = await fetch(url);
     const data = await r.json();
-    res.status(200).json(data.result);
+
+    if (data.status !== "OK") {
+      return res.status(500).json({ error: data.status });
+    }
+
+    return res.json(data.result);
   } catch (err) {
-    res.status(500).json({ error: "Google Places fetch failed" });
+    console.error("Google Places error:", err);
+    return res.status(500).json({ error: "Google fetch failed" });
   }
 }
+
