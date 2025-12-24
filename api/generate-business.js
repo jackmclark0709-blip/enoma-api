8e5d4ffe781b5511ba5c8b3b4b9a2d41e876eeba
Jack Clark <Jack.Clark@assayquant.com>
2025-12-23 22:25:33 -0500

    service role

--- api/generate-business.js
+++ api/generate-business.js
@@ -15,7 +15,7 @@ export const config = {
 
 const supabaseAdmin = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 /* --------------------------------------------------
